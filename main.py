15c2ec8dbe95730ff9fd6e9c0042709ab064b26b
Erik Fredericks <frederer@gvsu.edu>
2023-10-11 10:48:43 -0400

    updated run for cloud server

--- main.py
+++ main.py
@@ -877,4 +877,5 @@ if __name__ == '__main__':
     log = logging.getLogger("werkzeug")
     log.disabled = True
 
-    socketio.run(app)
+    # socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=8080)
